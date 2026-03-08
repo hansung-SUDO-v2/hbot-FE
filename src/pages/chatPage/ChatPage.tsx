e85ec0725ff93f3313c4c2fdd0d9d2e8f0ced7e8
Gamgyul <ryusangwan12@gmail.com>
2026-03-08 22:30:01 +0900

    feat: 라우팅 세팅 및 더미 페이지 추가

---
+++ src/pages/chatPage/ChatPage.tsx
@@ -0,0 +1,9 @@
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>채팅 페이지</h1>
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import RootLayout from '../layouts/RootLayout';
+import ChatPage from '../pages/chatPage/ChatPage';
+import MainPage from '../pages/mainPage/MainPage';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+      },
+      {
+        path: '/chat/:chatId',
+        element: <ChatPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
